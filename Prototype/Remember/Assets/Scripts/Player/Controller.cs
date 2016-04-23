93634341ba9a2f7f6d7c32c4ebaed5a6207a5d52
Hologuardian <namaeskram@gmail.com>
2016-04-23 05:53:00 -0400

    Modified controller, to have separate methods to simulate an event system inside a single class. Should probably change them to be virtual.

--- Prototype/Remember/Assets/Scripts/Player/Controller.cs
+++ Prototype/Remember/Assets/Scripts/Player/Controller.cs
@@ -4,12 +4,67 @@ using System.Collections;
 public class Controller : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+	    if(Input.GetKeyDown(KeyCode.W))
+        {
+            MoveForward();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MoveBackward();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveRight();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            LeftClick();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RightClick();
+        }
 	}
+
+    void MoveForward()
+    {
+
+    }
+
+    void MoveLeft()
+    {
+
+    }
+
+    void MoveBackward()
+    {
+
+    }
+
+    void MoveRight()
+    {
+
+    }
+
+    void LeftClick()
+    {
+
+    }
+
+    void RightClick()
+    {
+
+    }
 }
