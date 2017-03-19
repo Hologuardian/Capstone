940505d95ebefedd4e7fda2c721d89920afccf34
Liam Johnston <liamjohnston266@hotmail.ca>
2017-03-19 17:33:04 -0400

    Added music files and credit scene

---
+++ Inner Workings/Assets/Scripts/MenuScript/Credits.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+	void Start ()
+    {
+        StartCoroutine(swapScene(72.0f));
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private IEnumerator swapScene(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("Main Menu");
+    }
+}
