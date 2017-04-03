f3174490914d13f066cf418a0c5c5e96420a5215
Liam Johnston <liamjohnston266@hotmail.ca>
2017-04-03 13:59:09 -0400

    Health System

---
+++ Inner Workings/Assets/Scripts/BarScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BarScript : MonoBehaviour
+{
+    public float fillAmount;
+    public Image content;
+
+    public float maxValue { get; set; }
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    public float value
+    {
+        set
+        {
+            fillAmount = Map(value, 0, maxValue, 0, 1);
+        }
+    }
+
+	void Start ()
+    {
+        
+	}
+
+	void Update ()
+    {
+        handleBar();
+	}
+
+    public void handleBar()
+    {
+        content.fillAmount = Mathf.Lerp(content.fillAmount, fillAmount, Time.deltaTime * lerpSpeed);
+    }
+
+    public float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
---
+++ Inner Workings/Assets/Scripts/hpStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class hpStats
+{
+    [SerializeField]
+    private BarScript bar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currVal;
+
+    public float CurrVal
+    {
+        get
+        {
+            return currVal;
+        }
+
+        set
+        {
+            this.currVal = value;
+            bar.value = currVal;           
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            this.maxVal = value;
+            bar.maxValue = maxVal;
+        }
+    }
+
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+    }
+}
